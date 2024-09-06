93bc5500b9841d7ea044f9a27e169cef06a9b07b
rivumitter <rivumitter@gmail.com>
2024-09-06 18:23:15 +0530

    codes added

---
+++ src/strings/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+package strings;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"flower","flow","flight"};
+
+        //System.out.println("abc".indexOf("abc"));
+
+        System.out.println(longestCommonPrefix(arr));
+    }
+
+    private static String longestCommonPrefix(String[] arr) {
+
+        String pref = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            while (arr[i].indexOf(pref)!=0) {
+                pref = new StringBuilder(pref).substring(0, pref.length() - 1);
+            }
+        }
+
+        return pref;
+    }
+}
